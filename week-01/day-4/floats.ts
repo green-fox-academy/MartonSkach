f2e46f3eb1586a3630c25ca34f91fcc00b20986c
MartonSkach <martonskach@gmail.com>
2018-05-10 13:43:29 +0200

    Uploading floats tasks

---
+++ week-01/day-4/floats.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+export {};
+
+let a: number = 3.1415;
+console.log(a);
+
+let b: number = -1.5;
+console.log(b);
+
+let c: number = 0;
+console.log(c);
+
+let d: number = .5;
+console.log(d);
+
+let e: number = 5.;
+console.log(e);
