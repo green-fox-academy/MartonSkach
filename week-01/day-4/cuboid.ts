776b6939b783882d764cb1daba5f15781243b04d
MartonSkach <martonskach@gmail.com>
2018-05-10 15:59:48 +0200

    Uploading cuboid.ts

---
+++ week-01/day-4/cuboid.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+export {};
+
+// sides of the cuboid
+
+let a: number = 5;
+let b: number = 7;
+let c: number = 8;
+
+console.log('Surface Area: ' + (2 * a + 2 * b + 2 * c));
+console.log('Volume: ' + a * b * c);
