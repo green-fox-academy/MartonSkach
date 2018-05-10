a17c3504d0bb0fd5c3a4246ac46a2a67296a8cc6
MartonSkach <martonskach@gmail.com>
2018-05-10 17:04:52 +0200

    Added print-even.multiplication-table.ts

---
+++ week-01/day-4/multiplication-table.ts
@@ -0,0 +1,25 @@
+'use strict';
+export {};
+
+let number: number = 17;
+
+// Create a program that
+// prints the multiplication table with number
+//
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
+
+for (let multiplier: number = 0; multiplier <= 10; multiplier++) {
+    console.log(multiplier + " * " + number + ' = ' + (multiplier * number))
+}
