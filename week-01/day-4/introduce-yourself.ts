c405b89e5586e95ba9954b270d55bcc5a20d79b6
MartonSkach <martonskach@gmail.com>
2018-05-10 14:19:41 +0200

    Uploading introduce-yourself.ts

---
+++ week-01/day-4/introduce-yourself.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+export {};
+
+let name: string = 'Marton Skach';
+let age: number = 25;
+let height: number = 1.7;
+
+console.log('Your personal data:');
+console.log('Name: ' + name);
+console.log('Age: ' + age);
+console.log('Height: ' + height + ' m');
