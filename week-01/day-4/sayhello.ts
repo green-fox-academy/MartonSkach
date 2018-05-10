b7f820829ebb19682365b7781c39b9e3e25f967f
MartonSkach <martonskach@gmail.com>
2018-05-10 12:32:51 +0200

    Uploading Hello World exercises

---
+++ week-01/day-4/sayhello.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+// Printing to the terminal window
+console.log('Hello, World!');
+
+// Modify this program to greet you instead of the World!
+console.log('Hello, Marton!');
+
+// Humpty Dumpty riddle
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Humpty Dumpty had a great fall');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
+
+// Greetings maties!
+console.log('Hello, Attila!');
+console.log('Hello, Virag!');
+console.log('Hello, Gonba!');
+
+// Multi line comment
+/* 
+Yoda does Haiku
+Strong is the force within you
+Chubakka hairy
+*/
