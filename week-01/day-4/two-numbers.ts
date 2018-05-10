07f90fdf34233329fcfd5e32a78a3ff0f6dc335f
MartonSkach <martonskach@gmail.com>
2018-05-10 14:20:13 +0200

    Uploading two-numbers.ts

---
+++ week-01/day-4/two-numbers.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+export {};
+
+let a: number = 13;
+let b: number = 22;
+
+// Print the result of 13 added to 22
+console.log(a + b);
+
+// Print the result of 13 substracted from 22
+console.log(b + a);
+
+// Print the result of 22 multiplied by 13
+console.log(b * a);
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+console.log(b / a);
+
+// Print the reminder of 22 divided by 13
+console.log(b % a);
