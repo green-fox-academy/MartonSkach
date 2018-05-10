d107304314334229c1b2b122a2e9d88a93f32be6
MartonSkach <martonskach@gmail.com>
2018-05-10 13:42:43 +0200

    Uploading Boolean tasks

---
+++ week-01/day-4/boolean.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+// True, false statements
+console.log(true);
+console.log(false);
+
+// Negation
+console.log(!true);
+console.log(!false);
+
+// And
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+// Or
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+//Special characters and stuff for the end
+
+console.log('"Everything you can imagine is true." - Picasso');
+
+console.log('tooth' + 'brush');
+
+console.log('My favorite number is:' + 8);
