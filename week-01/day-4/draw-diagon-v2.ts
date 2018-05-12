f17989f8217c13abb7a8ee4d3bf82eece4a03db7
MartonSkach <martonskach@gmail.com>
2018-05-13 00:08:43 +0200

    added draw-diagon task

---
+++ week-01/day-4/draw-diagon-v2.ts
@@ -0,0 +1,24 @@
+
+export{};
+
+
+let lineCount: number = 6;
+
+for (let i: number = 0; i < lineCount; i++) {
+    let lineString: string = '';
+    if (i == 0 || i == lineCount - 1) {
+        for (let a: number = 1; a < lineCount; a++) {
+            lineString = lineString + '%';
+        }
+    } else {
+        for (let b: number = 1; b < lineCount; b++) {
+            if (b == 1 || b == lineCount-1 || b == i + 1) {
+                lineString = lineString + '%';
+            } else {
+                lineString = lineString + ' '
+            }
+        } 
+       
+    }
+    console.log(lineString);
+}
