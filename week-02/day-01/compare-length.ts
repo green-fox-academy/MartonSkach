917d084fbb2397a3d2ede5b8b224f2fc0bce98ed
MartonSkach <martonskach@gmail.com>
2018-05-14 13:36:05 +0200

    added compare-length.ts

---
+++ week-02/day-01/compare-length.ts
@@ -0,0 +1,8 @@
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+
+if (firstList.length > secondList.length) {
+    console.log('firstList is longer');
+} else {
+    console.log('secondList is longer');
+}
