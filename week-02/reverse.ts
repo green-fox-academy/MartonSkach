a8b2a5129133c8639c471992a30a1c768129a82b
MartonSkach <martonskach@gmail.com>
2018-05-15 10:44:27 +0200

    added reverse.ts

---
+++ week-02/reverse.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
+let splitReversed = reversed.split('');
+let correcterArray = splitReversed.reverse();
+console.log(correcterArray.join(''));
+
