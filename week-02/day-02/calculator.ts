4fa60d9ef0fec92acdaf7644aad4c86f737dd496
MartonSkach <martonskach@gmail.com>
2018-05-15 17:08:55 +0200

    added a calculator, but a primitive one

---
+++ week-02/day-02/calculator.ts
@@ -0,0 +1,43 @@
+'use strict';
+// Create a simple calculator application which does read the parameters from the standard input
+// and prints the result to the console.
+// It should support the following operations, create function called "calculate()" :
+// +, -, *, /, % and it should support two operands:
+// The format of the expressions must be: {operation} {operand} {operand}.
+// Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+// You should use the command line arguments to accept user input
+// It should work like this:
+// Start the program with "node calculator.js + 5 6"
+// If number of arguments are not correct, print some nice errors
+// Else print the result
+// Say goodbye
+
+let inputChars: any[] = ['-', 3, 5];
+
+function calculate(inputChars) {
+    if (inputChars[0] === '+' && inputChars.length === 3) {
+        return `${inputChars[1]} ${inputChars[0]} ${inputChars[2]} = ${inputChars[1]+inputChars[2]} \nGoodbye!`;
+
+    } else if (inputChars[0] === '-' && inputChars.length === 3) {
+        return `${inputChars[1]} ${inputChars[0]} ${inputChars[2]} = ${inputChars[1]-inputChars[2]} \nGoodbye!`;
+
+    } else if (inputChars[0] === '*' && inputChars.length === 3) {
+        return `${inputChars[1]} ${inputChars[0]} ${inputChars[2]} = ${inputChars[1]*inputChars[2]} \nGoodbye!`;
+
+    } else if (inputChars[0] === '/' && inputChars.length === 3) {
+        return `${inputChars[1]} ${inputChars[0]} ${inputChars[2]} = ${inputChars[1]/inputChars[2]} \nGoodbye!`;
+
+    } else if (inputChars[0] === '%' && inputChars.length === 3) {
+        return `${inputChars[1]} ${inputChars[0]} ${inputChars[2]} = ${inputChars[1]%inputChars[2]} \nGoodbye!`;
+
+    } else {
+        return `Something went wrong!`
+    }
+}
+
+
+//const args = process.argv.splice(2); // Just a helper for you to get started
+
+console.log(calculate(inputChars));
+
+//export = calculator;
