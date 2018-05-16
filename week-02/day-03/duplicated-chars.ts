b40b4b62066449594f9098860973024eaf321161
MartonSkach <martonskach@gmail.com>
2018-05-16 21:52:32 +0200

    added unfinished duplicated-chars excersie

--- week-02/day-03/duplicated-chars.ts
+++ week-02/day-03/duplicated-chars.ts
@@ -1,27 +1,30 @@
 'use strict';
-export {};
+export { };
 
 declare function require(path: string): any;
 const fs = require('fs');
 
 function removeDuplicate(filePath: string) {
-    try {
-        let tempString: string[] = fs.readFileSync(filePath, 'utf-8').split('');
-        /*for (let j: number = 0; j < tempString.length; j++) {
-            tempString[j].split('');
-        }*/
-        let reducedString: string[] = [];
-        let correctedText: string[] = []; 
-        for (let i: number = 0; i < tempString.length; i+2) {
+    let tempString: string[] = fs.readFileSync(filePath, 'utf-8').split('');
+    /*for (let j: number = 0; j < tempString.length; j++) {
+        tempString[j].split('');
+    }*/
+    let reducedString: string[] = [];
+    let correctedText: string[] = [];
+    for (let i: number = 0; i < tempString.length; i++) {
+        if (i % 2 === 0) {
             reducedString.push(tempString[i]);
         }
-        /* for (let j: number = 0; j < reducedString.length; j++) {
-            console.log(reducedString[j].join());
-        } */
-        console.log(reducedString);
-    } catch(error) {
-
     }
+    /* for (let j: number = 0; j < reducedString.length; j++) {
+        console.log(reducedString[j].join());
+    } */
+    let finalText: any = reducedString.toString();
+    console.log(finalText.join(','));
 }
 
-removeDuplicate('duplicated-chars.txt');
+try {
+    removeDuplicate('duplicated-chars.txt');
+} catch (error) {
+    console.log('LOFASZ')
+};
