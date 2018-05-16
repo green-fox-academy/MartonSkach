e3f16767237767da36864edf94a10bbf5c276589
MartonSkach <martonskach@gmail.com>
2018-05-16 15:09:06 +0200

    added logs.ts

---
+++ week-02/day-03/logs.ts
@@ -0,0 +1,39 @@
+'use strict';
+export{};
+
+declare function require(path: string): any;
+const fs = require('fs');
+
+function ipLister(filePath: string) {
+    try {
+        let tempArray: string[] = fs.readFileSync(filePath, 'utf-8').split('\r\n');
+        let containerArray: string[] = [];
+        for (let i: number = 0; i < tempArray.length; i++) {
+            containerArray.push(tempArray[i].substr(27,11));
+        } 
+        console.log(containerArray);
+    } catch(error) {
+        console.log(`File unreachable`);
+    }
+}
+
+function ratioCounter(filePath: string) {
+    try {
+        let tempArrayRatio: string[] = fs.readFileSync(filePath, 'utf-8').split('\r\n');
+        let containerPost: string[] = [];
+        let containerGet: string [] = [];
+        for (let j: number = 0; j < tempArrayRatio.length; j++) {
+            if (tempArrayRatio[j].substr(41, 4) === 'POST') {
+                containerPost.push(tempArrayRatio[j].substr(41, 4));
+            }
+            containerGet.push(tempArrayRatio[j].substr(41, 4));
+        }
+        console.log(containerGet.length / containerPost.length);
+
+    } catch(error) {
+        console.log(`File unreachable`)
+    }
+}
+
+ipLister('log.txt');
+ratioCounter('log.txt');
