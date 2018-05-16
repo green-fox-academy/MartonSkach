352b975abd44bc8c887940472f49c1d30420824c
MartonSkach <martonskach@gmail.com>
2018-05-16 11:35:01 +0200

    added count-lines.ts

---
+++ week-02/day-03/count-lines.ts
@@ -0,0 +1,16 @@
+'use strict';
+export {};
+
+declare function require(path: string): any;
+const fs = require('fs');
+
+function readFromFile(filePath: string) {
+    try {
+        let lineCounter: string = (fs.readFileSync(filePath, 'utf-8'));
+        console.log(lineCounter.split(`\r\n`).length);
+    } catch(error) {
+        console.log(0);
+    }
+}
+
+readFromFile('my-file.txt');
