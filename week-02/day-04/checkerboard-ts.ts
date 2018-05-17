2dc137dfe2c54e1b3f84518d043f7d860c0fb5fd
MartonSkach <martonskach@gmail.com>
2018-05-17 14:07:10 +0200

    checkerboard-ts.ts

---
+++ week-02/day-04/checkerboard-ts.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+let positionX: number = 0;
+let positionY: number = 0;
+
+function checkerBoard(x: number) {
+    for (let i: number = 0; i < 400 / x; i++) {
+        if (i % 2 === 0) {
+            positionX = 0;
+            for (let j: number = 0; j < 600 / x; j++) {
+                if (j % 2 === 0) {
+                    ctx.fillStyle = 'white';
+                    ctx.fillRect(positionX, positionY, x, x);
+                } else {
+                    ctx.fillStyle = 'black';
+                    ctx.fillRect(positionX, positionY, x, x);
+                }
+                positionX = positionX + x;
+            }
+        } else if (i % 2 != 0) {
+            positionX = 0;
+            for (let j: number = 0; j < 600 / x; j++) {
+                if (j % 2 === 0) {
+                    ctx.fillStyle = 'black';
+                    ctx.fillRect(positionX, positionY, x, x);
+                } else {
+                    ctx.fillStyle = 'white';
+                    ctx.fillRect(positionX, positionY, x, x);
+                }
+                positionX = positionX + x;
+            }
+        }
+        positionY = positionY + x;
+    }
+}
+
+checkerBoard(20);
