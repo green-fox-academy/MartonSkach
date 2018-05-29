82619120e2ac899394af9c87295c13e40e797e21
MartonSkach <martonskach@gmail.com>
2018-05-29 09:32:27 +0200

    updated tree.ts

--- week-03/day-05/tree.ts
+++ week-03/day-05/tree.ts
@@ -3,69 +3,29 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-class LineDraw {
-  startingX: number;
-  startingY: number;
-  endingX: number;
-  endingY: number;
-
-  constructor(startingX: number, startingY: number, endingX: number, endingY: number) {
-
-    this.startingX = startingX;
-    this.startingY = startingY;
-    this.endingX = endingX;
-    this.endingY = endingY;
-
-    ctx.strokeStyle = '#FFD900';
-    ctx.beginPath();
-    ctx.moveTo(startingX, startingY);
-    ctx.lineTo(endingX, endingY);
-    ctx.stroke();
-  }
-}
-
-let startingX: number = 0;
-let startingY: number = 0;
-let endingX: number = 0;
-let endingY: number = 0;
-
-function drawTree(n: number) {
+function draw(startX, startY, branchLength, angle, branchWidth) {
+  ctx.beginPath();
+  ctx.save();
   
-  if (n <= 1) {
-    return 1;
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = branchWidth;
+  ctx.translate(startX, startY);
+  ctx.rotate(angle * Math.PI/180);
+  ctx.moveTo(0, 0);
+  ctx.lineTo(0, -branchLength);
+  ctx.stroke();
+  
+  if(branchLength < 10) {
+    ctx.restore();
+    return;
   }
-  else {
-    
-
-    for (let i: number = 0; i < n ; i++) {
-      for (let j: number = 0; j < i; j++) {
-  }
- 
-
-    }
-  }
+  
+  draw(0, -branchLength, branchLength * 0.8, -20, branchWidth * 0.8);
+  draw(0, -branchLength, branchLength * 0.8, 0, branchWidth * 0.8);
+  draw(0, -branchLength, branchLength * 0.8, 20, branchWidth * 0.8);
+  
+  ctx.restore();
 }
 
+draw(canvas.width / 2, canvas.height * 0.9, canvas.height * 0.18, 0, 4)
 
-
-
-ctx.strokeStyle = '#FFD900';
-ctx.beginPath();
-ctx.moveTo(canvas.width * 0.5, canvas.height * 0.8);
-ctx.lineTo(canvas.width * 0.5, canvas.height * 0.7);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.moveTo(canvas.width * 0.5, canvas.height * 0.7);
-ctx.lineTo(canvas.width * 0.47, canvas.height * 0.62);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.moveTo(canvas.width * 0.5, canvas.height * 0.7);
-ctx.lineTo(canvas.width * 0.5, canvas.height * 0.6);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.moveTo(canvas.width * 0.5, canvas.height * 0.7);
-ctx.lineTo(canvas.width * 0.53, canvas.height * 0.62);
-ctx.stroke();
