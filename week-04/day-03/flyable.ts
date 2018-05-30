bb341609032a0b59b62a784bb1e34299e45d31b2
MartonSkach <martonskach@gmail.com>
2018-05-30 14:41:52 +0200

    added flyable.ts

---
+++ week-04/day-03/flyable.ts
@@ -0,0 +1,51 @@
+'use strict';
+
+import {Bird} from './zoo';
+
+interface Flyable {
+  land();
+  fly();
+  takeOff();
+}
+
+abstract class Vehicle {
+  name: string;
+  numberOfPassengers: number;
+  fuelLeft: number;
+  constructor(name: string, numberOfPassengers?: number, fuelLeft?: number) {
+    this.name = name;
+    this.numberOfPassengers = numberOfPassengers;
+    this.fuelLeft = fuelLeft;
+  }
+}
+
+class Helicopter extends Vehicle implements Flyable {
+  constructor(name: string, numberOfPassengers: number, fuelLeft: number) {
+    super(name, numberOfPassengers, fuelLeft);
+  }
+  takeOff() {
+    return `The ${this.name} is taking off.`
+  }
+  land() {
+    return `The ${this.name} is landing.`
+  }
+  fly() {
+    return `The ${this.name} is flying.`
+  }
+}
+
+export {Flyable};
+
+
+
+
+
+//------------------------------------------------------------------
+let helicopter = new Helicopter('AH-64 Apache', 1, 30);
+let bird = new Bird("Parrot");
+
+console.log(helicopter);
+console.log(helicopter.fly());
+console.log(bird);
+console.log(bird.fly());
+
