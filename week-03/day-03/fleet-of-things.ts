3b84d5d96c5b63cca9f605e0a22f3bbc67fd35d2
MartonSkach <martonskach@gmail.com>
2018-05-30 21:21:25 +0200

    updated fleet-of-things.ts

--- week-03/day-03/fleet-of-things.ts
+++ week-03/day-03/fleet-of-things.ts
@@ -1,12 +1,26 @@
-//*import {Thing} from "./thing";*/
-import {Fleet} from "./fleet";
+import { Thing } from "./thing";
+import { Fleet } from "./fleet";
 
 let fleet = new Fleet();
-//* crete a fleet of things to have this output:*/
+
+let thing1 = new Thing('Get milk');
+let thing2 = new Thing('Remove the obstacles');
+let thing3 = new Thing('Stand up');
+let thing4 = new Thing('Eat lunch');
+
+fleet.add(thing1);
+fleet.add(thing2);
+thing3.complete();
+fleet.add(thing3);
+thing4.complete();
+fleet.add(thing4);
+fleet.print();
+
+//---------------------------------------------------------------------------
+
+//* create a fleet of things to have this output:*/
 //* 1. [ ] Get milk */
 //* 2. [ ] Remove the obstacles */
 //* 3. [x] Stand up */
 //* 4. [x] Eat lunch */
 //* Hint: You have to create a Print method also */
-
-console.log(fleet);
