ce17387fade251a5294d160e7ef89ce60320d8d0
MartonSkach <martonskach@gmail.com>
2018-05-30 21:21:44 +0200

    updated thiing.ts

--- week-03/day-03/thing.ts
+++ week-03/day-03/thing.ts
@@ -1,13 +1,21 @@
 class Thing {
-    private name: string;
-    private completed: boolean;
+  private name: string;
+  private completed: boolean;
 
-    constructor(name: string){
-        this.name = name;
-    }
-    public complete() {
-        this.completed = true;
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
+  public complete() {
+    this.completed = true;
+  }
+
+  public identify(): string {
+    return this.name;
+  }
+
+  public sortCompleted(): boolean {
+    return this.completed
+  }
 }
 
-export {Thing};
+export { Thing };
