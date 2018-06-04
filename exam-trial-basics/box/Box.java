0153458649604716464eb7bea03e4b28e80b915b
MartonSkach <martonskach@gmail.com>
2018-06-04 14:36:11 +0200

    delete trial

--- exam-trial-basics/box/Box.java
+++
@@ -1,6 +0,0 @@
-public class Cuboid {
-  // Create a class that represents a cuboid:
-  // It should take its three dimensions as constructor parameters (numbers)
-  // It should have a method called `getSurface` that returns the cuboid's surface
-  // It should have a method called `getVolume` that returns the cuboid's volume
-}
--- exam-trial-basics/seconds/Seconds.java
+++
@@ -1,7 +0,0 @@
-public class Seconds {
-  public static void main(String[] args) {
-    // Create a function that takes a list as a parameter,
-    // and returns a new list with every second element from the orignal list
-    // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
-  }
-}
