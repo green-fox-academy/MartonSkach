c2688d44eb5aef1df0d270cf4708ceaf01f0c194
MartonSkach <martonskach@gmail.com>
2018-06-26 11:50:23 +0200

    added multiple-timeout.js

---
+++ week-07/Day-02/multiple-timeout.js
@@ -0,0 +1,6 @@
+'use strict'
+
+setTimeout(() => console.log('Apple'), 0);
+setTimeout(() => console.log('Pear'), 1000);
+setTimeout(() => console.log('Melon'), 3000);
+setTimeout(() => console.log('Grapes'), 5000);
