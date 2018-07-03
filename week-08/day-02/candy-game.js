a1a6cdd5dc06c6eeded4f5084f1443c5ddf170f3
MartonSkach <martonskach@gmail.com>
2018-07-03 15:28:28 +0200

    added candy-game task

---
+++ week-08/day-02/candy-game.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const body = document.querySelector('body');
+const candyCounter = document.querySelector('.candies');
+const lollypopCounter = document.querySelector('.lollypops');
+const candySpeed = document.querySelector('.speed');
+let numberOfCandies = 0;
+let secondCounter = 0;
+let totalCandiedGathered = 0;
+let makeItRain = 1;
+
+body.addEventListener('click', function (e) {
+  if (e.target.innerText === 'Create candies') {
+    numberOfCandies++;
+    totalCandiedGathered++;
+    candyCounter.innerText = numberOfCandies;
+  }
+  if (e.target.innerText === 'Buy Lollypops') {
+    if (numberOfCandies > 100) {
+      numberOfCandies -= 100;
+      candyCounter.innerText = numberOfCandies;
+      lollypopCounter.innerText += `🍭`;
+    }
+  }
+  if (e.target.innerText === 'Make candy rain') {
+    makeItRain += '0';
+  }
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  setInterval(() => {
+    let candyGenerator = (lollypopCounter.innerText.length / 2) * 10 * parseInt(makeItRain);
+    numberOfCandies += candyGenerator;
+    totalCandiedGathered += candyGenerator;
+    candyCounter.innerText = numberOfCandies;
+    secondCounter++;
+    candySpeed.innerText = totalCandiedGathered / secondCounter;
+  }, 1000);
+});
