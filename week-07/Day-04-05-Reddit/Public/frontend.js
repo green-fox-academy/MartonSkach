763bf3f67fdf9919012cec3913e9224df9324794
MartonSkach <martonskach@gmail.com>
2018-07-05 15:45:30 +0200

    added delete function button

--- week-07/Day-04-05-Reddit/Public/frontend.js
+++ week-07/Day-04-05-Reddit/Public/frontend.js
@@ -13,29 +13,71 @@ const renderPage = () => {
     tr.classList.add('tableRow');
     table.appendChild(tr);
 
-    const scoreHolder = document.createElement('td')
-    const mainTextHolder = document.createElement('td')
+    const scoreHolderTd = document.createElement('td');
+    scoreHolderTd.classList.add('scoreHolderTd');
+    const mainTextHolder = document.createElement('td');
 
 
-    scoreHolder.innerHTML = `<div class='voteBar'>
+    scoreHolderTd.innerHTML = `<div class='voteBar'>
     <div class="voteUpButton" id="${element.post_id}"></div>
     <div class="scoreHolder"><p id="score${element.post_id}">${element.post_score}</p></div>
     <div class="voteDownButton" id="${element.post_id}"></div>
     </div>`;
-    
+
     mainTextHolder.innerHTML = `<div class="mainTextHolder">
     <a href="${element.post_url}" class="url_text"><p class="title_text">${element.post_title}</p></a>
     <p class="owner_name">Submitted by: ${element.post_owner}</p>
     <div class='postFunctions'>
-    <p class='deleteFunction'>delete</p>
-    <p class='modifyFunction'>modify</p>
+    <p class='deleteFunction' id='${element.post_id}'>delete</p>
+    <p class='modifyFunction'id='${element.post_id}'>modify</p>
     </div>
     </div>`;
-    
-    tr.appendChild(scoreHolder);
+
+    tr.appendChild(scoreHolderTd);
     tr.appendChild(mainTextHolder);
-    
+
   });
+
+  const downVote = document.querySelectorAll('.voteDownButton');
+  downVote.forEach(element => {
+    element.addEventListener('click', () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("PUT", `${host}/posts/${element.id}/downvote`, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send();
+      document.querySelector(`#score${element.id}`).innerText -= 1;
+      event.preventDefault();
+    });
+  });
+
+  const upVote = document.querySelectorAll('.voteUpButton');
+  upVote.forEach(element => {
+    element.addEventListener('click', () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("PUT", `${host}/posts/${element.id}/upvote`, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send();
+      let upVotedScore = document.querySelector(`#score${element.id}`).innerText;
+      upVotedScore = parseInt(upVotedScore) + 1;
+      document.querySelector(`#score${element.id}`).innerText = upVotedScore;
+      event.preventDefault();
+    });
+  });
+
+  const deletePostButton = document.querySelectorAll('.deleteFunction');
+  console.log(deletePostButton);
+  deletePostButton.forEach(element => {
+    element.addEventListener('click', () => {
+      if (confirm('Are you sure you want to delete this post?')) {
+        const xhr = new XMLHttpRequest();
+        xhr.open("DELETE", `${host}/posts/${element.id}`, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+        event.preventDefault();
+      };
+    });
+  });
+
 };
 
 http.onload = () => {
@@ -43,32 +85,6 @@ http.onload = () => {
 };
 
 http.send();
-
-const downVote = document.querySelectorAll('.voteDownButton');
-downVote.forEach(element => {
-  element.addEventListener('click', () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("PUT", `${host}/posts/${element.id}/downvote`, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send();
-    document.querySelector(`#score${element.id}`).innerText -= 1;
-    event.preventDefault();
-  });
-});
-
-const upVote = document.querySelectorAll('.voteUpButton');
-upVote.forEach(element => {
-  element.addEventListener('click', () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("PUT", `${host}/posts/${element.id}/upvote`, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send();
-    let upVotedScore = document.querySelector(`#score${element.id}`).innerText;
-    upVotedScore = parseInt(upVotedScore) + 1;
-    document.querySelector(`#score${element.id}`).innerText = upVotedScore;
-    event.preventDefault();
-  });
-});
 
 const addNewPostButton = document.querySelector('.addNewPostButton');
 let titleInput = document.querySelector('.titleInput');
@@ -87,15 +103,12 @@ if (!postOwnerInput.value) {
   postOwnerInput.value = "";
 }
 
-
-
 addNewPostButton.addEventListener('click', () => {
   const xhr = new XMLHttpRequest();
   xhr.open("POST", `${host}/posts`, true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onload = () => {
     const resp = JSON.parse(xhr.responseText);
-    console.log(resp);
   }
   xhr.send(JSON.stringify({
     post_title: titleInput.value,
@@ -105,11 +118,9 @@ addNewPostButton.addEventListener('click', () => {
   const xhrget = new XMLHttpRequest();
   xhrget.open("GET", `${host}/posts`, true);
   xhrget.setRequestHeader('Content-Type', 'application/json');
-  const table = document.querySelector('table');
-  table.innerHTML = '';
-  console.log(`${table.innerHTML}`);
   titleInput.value = '';
   urlInput.value = '';
   postOwnerInput.value = '';
-  renderPage();
 });
+
+
