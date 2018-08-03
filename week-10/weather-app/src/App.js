5a3bc2160c4fd2a6e9aa13a011a6735be327f832
MartonSkach <martonskach@gmail.com>
2018-08-03 10:56:55 +0200

    weather app updated

--- week-10/weather-app/src/App.js
+++ week-10/weather-app/src/App.js
@@ -46,20 +46,33 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Title />
-        <Form getWeather={this.getWeather} />
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
-        />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Title />
+                </div>
+                <div className="col-sx-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 };
 
 export default App;
+
 
--- week-10/weather-app/src/components/title.js
+++ week-10/weather-app/src/components/title.js
@@ -15,8 +15,8 @@ class Title extends React.Component {
 
 const Title = () => (
   <div>
-    <h1>Weather Finder</h1>
-    <p>Find out temperature, conditions and more...</p>
+    <h1 className="title-container__title">Weather Finder</h1>
+    <h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
   </div>
 )
 
